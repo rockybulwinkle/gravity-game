341d617aae2b0420f80dfea72bbd872c207cd0d0
rockybulwinkle <rockybulwinkle@94fa56c9-a949-3c6a-5cca-d084cfaeaa66>
2011-10-03 20:28:28 +0000

    don't need this

--- trunk/Gravity/build/physics.d
+++
@@ -1,8 +0,0 @@
-physics.o: \
- c:/Users/hopwoocp/Documents/WiiProjects2/helloworld/source/physicsFuncs/physics.c \
- c:/Users/hopwoocp/Documents/WiiProjects2/helloworld/source/physicsFuncs/../definitions.h \
- c:/Users/hopwoocp/Documents/WiiProjects2/helloworld/source/physicsFuncs/physics.h
-
-c:/Users/hopwoocp/Documents/WiiProjects2/helloworld/source/physicsFuncs/../definitions.h:
-
-c:/Users/hopwoocp/Documents/WiiProjects2/helloworld/source/physicsFuncs/physics.h:
--- trunk/Gravity/build/vector.d
+++
@@ -1,5 +0,0 @@
-vector.o: \
- c:/Users/hopwoocp/Documents/WiiProjects2/helloworld/source/VectorCalculus/vector.c \
- c:/Users/hopwoocp/Documents/WiiProjects2/helloworld/source/VectorCalculus/../definitions.h
-
-c:/Users/hopwoocp/Documents/WiiProjects2/helloworld/source/VectorCalculus/../definitions.h:
